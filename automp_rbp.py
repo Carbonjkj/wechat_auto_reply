ac7f71d9edac763db0ec036fdd9d926268774273
Carbonjkj <carbonjkj@hotmail.com>
2017-03-14 20:59:25 +0100

    Carbon

--- automp_rbp.py
+++ automp_rbp.py
@@ -4,7 +4,7 @@ import itchat as wc
 import copy
 import time
 import threading
-
+import logging
 # 获取信息的线程
 class get_text_msg(threading.Thread):
     # 初始化 收到的信息，收到小冰的信息，小冰的用户名
@@ -30,11 +30,11 @@ class get_text_msg(threading.Thread):
                                 if msg['FromUserName'] == wc.originInstance.storageClass.userName:
                                     if '开始你的表演' in msg['Content']:
                                         autoreply = True
-                                        print('表演开始咯')
+                                        logging.info('表演开始咯')
                                         wc.send_msg('表演开始', self.xb)
                                     if '停止你的表演' in msg['Content']:
                                         autoreply = False
-                                        print('表演谢幕了')
+                                        logging.info('表演谢幕了')
                                         wc.send_msg('表演谢幕', self.xb)
                                 elif '@@' not in msg['FromUserName'] and '@' in msg['FromUserName'] :
                                     # 获取不是小冰的信息
@@ -49,7 +49,7 @@ class get_text_msg(threading.Thread):
                         except IndexError:
                             pass
             except:
-                print('Getting module face a problem')
+                logging.warning('Getting module face a problem')
                 pass
 
 
@@ -69,7 +69,7 @@ class reply_msg(threading.Thread):
                     msg = copy.deepcopy(self.mi[0])
                     self.mi.pop(0)
                     user = wc.search_friends(userName=msg['FromUserName'])
-                    print('[%s] %s - %s' % (
+                    logging.info('[%s] %s - %s' % (
                         time.ctime(time.time()), msg['Content'], user['NickName']))
                     wc.send_msg(msg['Content'], self.xb)
                     del self.mo[:]
@@ -84,13 +84,13 @@ class reply_msg(threading.Thread):
                     if replied == False:
                         last_reply = copy.deepcopy(self.mo[-1])
                         wc.send_msg('%s' % last_reply['Content'], msg['FromUserName'])
-                        print('[%s] %s - 小冰' % (
+                        logging.info('[%s] %s - 小冰' % (
                             time.ctime(time.time()), last_reply['Content']))
                 else:
                     time.sleep(0.2)
             except:
                 time.sleep(0.2)
-                print('reply module face a problem')
+                logging.warning('reply module face a problem')
                 pass
 
 
@@ -100,12 +100,11 @@ def find_xiaobing():
     while True:
         try:
             xiaobing_name = wc.search_mps(name='小冰')[0]['UserName']
-
-            print('小冰找到啦！')
+            logging.info('小冰找到啦！')
             break
         except:
             if time.time() - time_out > 15:
-                print('小冰不见啦！20秒后重试')
+                logging.info('小冰不见啦！请关注小冰公众号，20秒后重试')
                 time.sleep(20)
             pass
     return xiaobing_name
@@ -113,9 +112,11 @@ def find_xiaobing():
 if __name__ == '__main__':
     MSGIN = []
     MSGOUT = []
+    logging.basicConfig(filename='autoreply.log', level=logging.INFO)
     wc.auto_login(enableCmdQR=2)
     XB = find_xiaobing()
-
+    logging.info('开始自动回复，请在随意对话框输入，开始你的表演')
+    logging.info('结束自动回复，请在随意对话框输入，结束你的表演')
     t1 = get_text_msg(MSGIN, MSGOUT, XB)
     t1.start()
 
